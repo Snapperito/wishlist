e2126fc1b775156a4d222bf81eb9af40d5682dba
snapperito <milesreed27@gmail.com>
2021-04-14 00:17:00 -0700

    themes (thank you tropical for help) mobile support spoon :spoon:

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const openMenu = document.querySelector('#show-menu')
 const hideMenuIcon = document.querySelector('#hide-menu')
 const sideMenu = document.querySelector('#nav-menu')
+const button = document.querySelector('.button-selector');
+const theme = localStorage.getItem('theme');
+const getSystemDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
 
 openMenu.addEventListener('click', function() {
     sideMenu.classList.add('active')
@@ -9,3 +13,24 @@ openMenu.addEventListener('click', function() {
 hideMenuIcon.addEventListener('click', function() {
     sideMenu.classList.remove('active')
 })
+
+if (theme == 'dark') {
+    document.documentElement.classList.add('theme-dark');
+} else if (theme == 'light') {
+    document.documentElement.classList.add('theme-light');
+} else if (getSystemDarkMode.matches) {
+    localStorage.setItem('theme', 'dark');
+    document.documentElement.classList.add('theme-dark');
+} else {
+    localStorage.setItem('theme', 'light');
+    document.documentElement.classList.add('theme-light');
+}
+button.addEventListener('click', () => {
+    if (theme == 'light') {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+    document.documentElement.classList.toggle('theme-light');
+    document.documentElement.classList.toggle('theme-dark');
+});
